61babb0234b7bf1f8702bbd2041f79516a0b5753
Ben Williams <ben@negativeeddy.com>
2016-07-03 20:20:55 -0500

    added comments

--- src/ZMachine/ZObjectTable.cs
+++ src/ZMachine/ZObjectTable.cs
@@ -14,6 +14,11 @@ namespace ZMachine
             Load(data, loadAddress);
         }
 
+        /// <summary>
+        /// Populates the Object collection from memory
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="loadAddress"></param>
         private void Load(byte[] data, int loadAddress)
         {
             // v1-3 object size is 9, if v4+ then object size is 14 
@@ -40,8 +45,15 @@ namespace ZMachine
             }
         }
 
+        /// <summary>
+        /// All of the ZObjects in the machine
+        /// </summary>
         public List<ZObject> Objects { get; } = new List<ZObject>(256);
 
+        /// <summary>
+        /// Creates a string representation of the entire object tree in memory
+        /// </summary>
+        /// <returns>a string representing the object tree</returns>
         public string DumpObjectTree()
         {
             StringBuilder sb = new StringBuilder();
@@ -52,6 +64,12 @@ namespace ZMachine
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Prints the specified object and its children to a string builder
+        /// </summary>
+        /// <param name="current">the object to print</param>
+        /// <param name="level">the level of indention to print at</param>
+        /// <param name="sb">the string builder object which contains the printed object tree</param>
         private void PrintObject(ZObject current, int level, StringBuilder sb)
         {
             for (int i = 0; i < level; i++) { sb.Append("-----"); }    // do the indention
