dbb78ad6143d5910c9739e6c0698df45ff7dc2ea
Ben Williams <ben@negativeeddy.com>
2018-06-22 00:14:27 -0500

    Replace old tuples with new valuetuple (#3)
    * replace Tuple<T>
    
    * replace tuples

--- src/NegativeEddy.Leaflet.Tests/Instructions/ZOpcodeDefinitionTests.cs
+++ src/NegativeEddy.Leaflet.Tests/Instructions/ZOpcodeDefinitionTests.cs
@@ -9,25 +9,25 @@ namespace NegativeEddy.Leaflet.Instructions.Tests
         [TestMethod]
         public void OpcodeDefinitionConstructorTests()
         {
-            var data = new Tuple<string, OperandCountType, ushort>[] {
-                new Tuple<string, OperandCountType, ushort>("OP2_2", OperandCountType.OP2, 2),
-                new Tuple<string, OperandCountType, ushort>("OP2_10", OperandCountType.OP2, 10),
-                new Tuple<string, OperandCountType, ushort>("OP1_132", OperandCountType.OP1, 4),
-                new Tuple<string, OperandCountType, ushort>("OP0_177", OperandCountType.OP0, 1),
-                new Tuple<string, OperandCountType, ushort>("EXT_1", OperandCountType.EXT, 1),
-                new Tuple<string, OperandCountType, ushort>("EXT_21", OperandCountType.EXT, 21),
-                new Tuple<string, OperandCountType, ushort>("VAR_228", OperandCountType.VAR, 4),
+            var data = new (string opcodeName, OperandCountType countType, ushort opcodeNumber)[] {
+                ("OP2_2", OperandCountType.OP2, 2),
+                ("OP2_10", OperandCountType.OP2, 10),
+                ("OP1_132", OperandCountType.OP1, 4),
+                ("OP0_177", OperandCountType.OP0, 1),
+                ("EXT_1", OperandCountType.EXT, 1),
+                ("EXT_21", OperandCountType.EXT, 21),
+                ("VAR_228", OperandCountType.VAR, 4),
             };
 
             foreach (var item in data)
             {
                 // validate values are set correctly
-                var first = new OpcodeIdentifier(item.Item1);
-                Assert.AreEqual(item.Item2, first.OperandCount);
-                Assert.AreEqual(item.Item3, first.OpcodeNumber);
+                var first = new OpcodeIdentifier(item.opcodeName);
+                Assert.AreEqual(item.countType, first.OperandCount);
+                Assert.AreEqual(item.opcodeNumber, first.OpcodeNumber);
 
                 // validate equivalence
-                var second = new OpcodeIdentifier(item.Item2, item.Item3);
+                var second = new OpcodeIdentifier(item.countType, item.opcodeNumber);
                 Assert.AreEqual(second, first);
             }
         }
