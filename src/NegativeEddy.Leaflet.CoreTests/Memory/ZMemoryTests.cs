4de1d468a4c715640238f5320c4bc898be8647e0
Ben Williams <ben@negativeeddy.com>
2019-09-26 17:10:14 -0500

    simplifying test

--- src/NegativeEddy.Leaflet.CoreTests/Memory/ZMemoryTests.cs
+++ src/NegativeEddy.Leaflet.CoreTests/Memory/ZMemoryTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace NegativeEddy.Leaflet.Tests.Memory
 {
@@ -14,32 +15,31 @@ namespace NegativeEddy.Leaflet.Tests.Memory
         [TestMethod()]
         public void SaveAndLoadInterpreterState()
         {
-            Stream state = new MemoryStream();
+            var stateStream = new MemoryStream();
             string filename = @"GameFiles\minizork.z3";
-            state = RunInterpreter(filename, state, new string[0]);
-            state = RunInterpreter(filename, state, new string[] { "open mailbox" });
-            state = RunInterpreter(filename, state, new string[] { "get leaflet" });
-            state = RunInterpreter(filename, state, new string[] { "read leaflet" });
+            RunInterpreter(filename, stateStream, new string[0]);
+            RunInterpreter(filename, stateStream, new string[] { "open mailbox" });
+            RunInterpreter(filename, stateStream, new string[] { "get leaflet" });
+            RunInterpreter(filename, stateStream, new string[] { "read leaflet" });
         }
 
-        private Stream RunInterpreter(string gameFile, Stream state, IEnumerable<string> nextInput)
+        private void RunInterpreter(string gameFile, MemoryStream stateStream, IEnumerable<string> nextInput)
         {
-            Interpreter zMachine = new Interpreter();
-
-            zMachine.Input = new InputFeeder(nextInput);
-            zMachine.DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Off;
+            Interpreter zMachine = new Interpreter
+            {
+                Input = new InputFeeder(nextInput),
+                DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Off
+            };
 
             if (nextInput.Count() == 0)
             {
                 // reset the state of the vm
-                using (var stream = System.IO.File.OpenRead(gameFile))
-                {
-                    zMachine.LoadStory(stream);
-                }
+                using var stream = System.IO.File.OpenRead(gameFile);
+                zMachine.LoadStory(stream);
             }
             else
             {
-                zMachine.ReadState(state);
+                zMachine.ReadState(stateStream);
             }
 
             using (zMachine.Output.Subscribe(x => Trace.Write(x)))
@@ -54,10 +54,9 @@ namespace NegativeEddy.Leaflet.Tests.Memory
                 }
             }
 
-            MemoryStream newState = new MemoryStream();
-            zMachine.WriteState(newState);
-            newState.Seek(0, SeekOrigin.Begin);
-            return newState;
+            stateStream.Seek(0, SeekOrigin.Begin);
+            zMachine.WriteState(stateStream);
+            stateStream.Seek(0, SeekOrigin.Begin);
         }
     }
 }
