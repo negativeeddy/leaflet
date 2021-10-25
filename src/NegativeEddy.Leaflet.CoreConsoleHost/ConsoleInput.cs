57ab4d9008de2bd37e83acf2645dc38af78a20c7
Ben Williams <ben@negativeeddy.com>
2021-10-24 21:28:10 -0500

    using implicit namespaces and file scoped namespaces

--- src/NegativeEddy.Leaflet.CoreConsoleHost/ConsoleInput.cs
+++ src/NegativeEddy.Leaflet.CoreConsoleHost/ConsoleInput.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using NegativeEddy.Leaflet.IO;
+﻿using NegativeEddy.Leaflet.IO;
 
-namespace NegativeEddy.Leaflet.CoreConsoleHost
+namespace NegativeEddy.Leaflet.CoreConsoleHost;
+
+class ConsoleInput : IZInput
 {
-    class ConsoleInput : IZInput
-    {
-        public char ReadChar() => Console.ReadKey().KeyChar;
+    public char ReadChar() => Console.ReadKey().KeyChar;
 
-        public string ReadLine() => Console.ReadLine();
-    }
+    public string ReadLine() => Console.ReadLine();
 }
--- src/NegativeEddy.Leaflet.CoreConsoleHost/Program.cs
+++ src/NegativeEddy.Leaflet.CoreConsoleHost/Program.cs
@@ -1,42 +1,38 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
 
-namespace NegativeEddy.Leaflet.CoreConsoleHost
+namespace NegativeEddy.Leaflet.CoreConsoleHost;
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main(string[] args)
+        var zm = new Interpreter
         {
-            var zm = new Interpreter
-            {
-                Input = new ConsoleInput(),
-                DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Verbose,
-            };
+            Input = new ConsoleInput(),
+            DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Verbose,
+        };
 
-            zm.Output.Subscribe(x => Console.Write(x));
-            zm.Diagnostics.Subscribe(x => Debug.Write(x));
+        zm.Output.Subscribe(x => Console.Write(x));
+        zm.Diagnostics.Subscribe(x => Debug.Write(x));
 
-            string filename = Path.Combine("GameFiles", "minizork.z3");
-            using (var stream = File.OpenRead(filename))
-            {
-                zm.LoadStory(stream);
-            }
-            Console.WriteLine($"Gamefile Version {zm.MainMemory.Header.Version}");
-
-            //DumpObjectTree(zm);
-
-            //DumpObjects(zm);
-
-            RunGame(zm);
+        string filename = Path.Combine("GameFiles", "minizork.z3");
+        using (var stream = File.OpenRead(filename))
+        {
+            zm.LoadStory(stream);
         }
+        Console.WriteLine($"Gamefile Version {zm.MainMemory.Header.Version}");
 
-        private static void RunGame(Interpreter zm)
+        //DumpObjectTree(zm);
+
+        //DumpObjects(zm);
+
+        RunGame(zm);
+    }
+
+    private static void RunGame(Interpreter zm)
+    {
+        while (zm.IsRunning)
         {
-            while (zm.IsRunning)
-            {
-                zm.ExecuteCurrentInstruction();
-            }
+            zm.ExecuteCurrentInstruction();
         }
     }
 }
