391f3657ab41b4462e51f19c921d0194fc73be42
Ben Williams <ben@negativeeddy.com>
2021-10-24 21:33:50 -0500

    simplify startup with top level statements

--- src/NegativeEddy.Leaflet.CoreConsoleHost/Program.cs
+++ src/NegativeEddy.Leaflet.CoreConsoleHost/Program.cs
@@ -1,38 +1,28 @@
-﻿using System.Diagnostics;
+﻿using NegativeEddy.Leaflet;
+using NegativeEddy.Leaflet.CoreConsoleHost;
+using System.Diagnostics;
 
-namespace NegativeEddy.Leaflet.CoreConsoleHost;
-public class Program
+var zm = new Interpreter
 {
-    public static void Main()
-    {
-        var zm = new Interpreter
-        {
-            Input = new ConsoleInput(),
-            DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Verbose,
-        };
+    Input = new ConsoleInput(),
+    DiagnosticsOutputLevel = Interpreter.DiagnosticsLevel.Verbose,
+};
 
-        zm.Output.Subscribe(x => Console.Write(x));
-        zm.Diagnostics.Subscribe(x => Debug.Write(x));
+zm.Output.Subscribe(x => Console.Write(x));
+zm.Diagnostics.Subscribe(x => Debug.Write(x));
 
-        string filename = Path.Combine("GameFiles", "minizork.z3");
-        using (var stream = File.OpenRead(filename))
-        {
-            zm.LoadStory(stream);
-        }
-        Console.WriteLine($"Gamefile Version {zm.MainMemory.Header.Version}");
+string filename = Path.Combine("GameFiles", "minizork.z3");
+using (var stream = File.OpenRead(filename))
+{
+    zm.LoadStory(stream);
+}
+Console.WriteLine($"Gamefile Version {zm.MainMemory.Header.Version}");
 
-        //DumpObjectTree(zm);
+//DumpObjectTree(zm);
 
-        //DumpObjects(zm);
+//DumpObjects(zm);
 
-        RunGame(zm);
-    }
-
-    private static void RunGame(Interpreter zm)
-    {
-        while (zm.IsRunning)
-        {
-            zm.ExecuteCurrentInstruction();
-        }
-    }
+while (zm.IsRunning)
+{
+    zm.ExecuteCurrentInstruction();
 }
